9c72d312ec781b1d86198ee36916b3f06bd1908a
lawsi08 <lawsi08@yahoo.co.uk>
2025-03-13 13:48:40 +0000

    next.config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src')
+    };
+    return config;
+  }
+};
